aca9ea2b1b73a429bf4b070bfe18eff6e6079f31
comarkgh8@gmail.com <comarkgh8@gmail.com>
2018-02-02 12:00:52 +0100

    added validations to community model

--- app/models/community.rb
+++ app/models/community.rb
@@ -4,6 +4,14 @@ class Community < ActiveRecord::Base
   
   #include Operation modules when needed
   
+  attr_accessor :login_pwd
+  
+  # password is at least 8 long, contains one letter, one number
+  LOGIN_PWD_REGEX = /\A^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}\z/
+  validates :name, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
+  validates :login_pwd, :confirmation => true
+  validates_format_of :login_pwd, :with => LOGIN_PWD_REGEX, :on => :create
+  
 end
 
 
