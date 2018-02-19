2113fd5bc02643bb2265d31bb033f5ef905c6f82
comarkgh8 <comarkgh8@gmail.com>
2018-02-19 18:03:12 +0100

    if lggd in res chcks cmmnty pg, can edit artcl; next: edit res/show pg

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -4,22 +4,26 @@ class CommunitiesController < ApplicationController
   def show
     id = params[:id] # retrieve community ID from URI route
     @commune = Community.find(id) # look up commune by unique ID
+
+    # get the current logged in resident (if any)
+    @current_resident = current_user
+    
     # will render app/views/communities/show.<extension> by default
   end
-  
+
   def new
     # want to be able to search
     # or create new
     @commune=Community.new
   end
-  
+
   def index
     # want to be able to search
     # or create new
     @commune=Community.new
     @communes = Community.all
   end
-  
+
   def create
     @commune = Community.new(community_params)
     if @commune.save
@@ -31,15 +35,15 @@ class CommunitiesController < ApplicationController
     end
 
   end
-  
+
   def show
     id = params[:id] # retrieve ID from URI route
     @commune = Community.find(id) # look up community by unique ID
     # will render app/views/communities/show.<extension> by default
   end
-  
+
   private
-  
+
       def community_params
         params.require(:commune).permit(:name,
                                         :password,
@@ -48,5 +52,5 @@ class CommunitiesController < ApplicationController
                                         :street,
                                         :number)
       end
- 
+
 end
