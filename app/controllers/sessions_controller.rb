9d4ef9e35d8f853f366ebf3367577786826ac949
comarkgh8 <comarkgh8@gmail.com>
2018-03-03 12:05:56 +0100

    debugged login pg; next try pos login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,28 +4,33 @@ class SessionsController < ApplicationController
 
   def new
 
-
   end
 
   def create
 
-    users = Resident.where(name: params[:session][:name])
-    if users.size > 1
+    @users = Resident.where(name: params[:session][:name])
+    if @users.size > 1
       @commune_list = Array.new
-      users.each do |res|
+      @users.each do |res|
         res_commune_id = res.community_id
         @commune_list << Community.find(res_commune_id)
       end
     end
 
-    if users.size > 1
+
+    if @users.size > 1
       # get commune of resident from page
       commune_id = params[:commune_id]
       @commune = Community.find(commune_id)
       # get the desired user
       @user = Resident.where(name: params[:session][:name], community_id: commune_id)
-      # see if can authenticate the user
-      if @user && @user.authenticate(params[:session][:password])
+
+    else
+      @user=@users.first
+    end
+
+    # see if can authenticate the user
+    if @user && @user.authenticate(params[:session][:password])
         # Log the user in (save res_id and comm_id
         log_in @user
         # and redirect to the user's show page.
@@ -33,8 +38,6 @@ class SessionsController < ApplicationController
       else
         flash.now[:danger] = 'Invalid email/password combination'
         render 'new'
-      end
-
     end
 
   end
